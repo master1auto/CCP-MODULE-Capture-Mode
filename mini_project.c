9f56e9a497fbfeca8d7e153ad2697ef5e87fad72
aymenauto <meissa.aymen92@gmail.com>
2016-01-05 10:21:53 +0100

    Update mini_project.c

--- mini_project.c
+++ mini_project.c
@@ -1,5 +1,31 @@
-void main() {
-  T1CON = 1               ' Set timer TMR1
-PIR1.TMR1IF = 0         ' Reset bit TMR1IF
-
+unsigned A ;
+unsigned B ;
+unsigned int time1 ;
+unsigned int time2 ;
+char i=0 ;
+void config(){
+Intcon=0xc0 ;
+T1con=0x30 ;
+ccp1con=0x04 ;
 }
+void capturstart(){
+pie1=0x40 ;
+TMR1on_bit=1 ;
+}
+void interrupt (){
+i++ ;
+if(i==1) ;
+time1=ccpr1l+(ccpr1h*256) ;
+if(i==2) ;
+time2=ccpr1l+(ccpr1h*256) ;
+TMR1on_bit=0 ;
+ }
+void main() {
+  T1CON = 1 ;
+PIR1.TMR1IF = 0 ;
+ pir1=0x04 ;
+ config();
+ if(i==2) ;
+ A=time2-time1 ;
+ B=A*0.4 ;
+ }
